2e9334dc5d2343add1a8909a61383479d01e1071
Petar Yovkov <petar@sgtautotransport.net>
2024-04-20 23:04:23 +0300

    reinstalled

--- nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -146,8 +146,8 @@ return {
         html = { filetypes = { "html", "twig", "hbs" } },
         -- phpactor = {},
         tailwindcss = {},
-        volar = {capabilities = capabilities},
-        intelephense = { capabilities = capabilities },
+        volar = {},
+        intelephense = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
